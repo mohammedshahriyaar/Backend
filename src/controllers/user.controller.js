6b15291bd5a82a1b0390f90db474ea0b872c301e
Mohammed Shahriyaar <mohammedshahriyaar@gmail.com>
2024-03-22 17:36:17 +0530

    getwatch history endpoint sub pipelines added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -558,6 +558,61 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
 
 })
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    
+    const user = await User.aggregate(
+        [
+            {
+                $match:{
+                    _id: new mongoose.Types.ObjectId(req.user?._id)
+                }
+            },
+            {
+                $lookup:{
+                    from:"videos",
+                    localField:"watchHistory",
+                    foreignField:"_id",
+                    as:"watchHistory",
+                    pipeline:[
+                        {  //subpipeline
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    //for convienience at front end
+
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    } 
+                 ]             
+                    
+                }
+            }
+        ]
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user[0].watchHistory,"Watch Histiory fetched success")
+    )
+})
 
 
 
@@ -570,5 +625,4 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
 
 
 
-
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
